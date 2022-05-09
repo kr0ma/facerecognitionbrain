7ffad71ba4570df801adb5dc823745744d10477c
Karim Romagnani <karim.romagnani@gmail.com>
2022-05-09 20:12:09 +0200

    added Navigation

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
+import React from 'react'; 
+import Navigation from './components/navigation/Navigation';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <Navigation />
+      {/* {<Logo />
+      <ImageLinkForm/>
+      <FaceRecognition/>} */}
     </div>
   );
 }
